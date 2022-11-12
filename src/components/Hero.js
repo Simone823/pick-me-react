0e044351ec750bce8ad413aa31b6e27ba2452bff
Simone823 <simone.daglio823@outlook.it>
2022-11-13 00:52:47 +0100

    fix align self

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ function Hero() {
             <h1 className='font-bold text-5xl sm:text-7xl mb-16'>The easiest way to buy Photos as NFT.</h1>
             <a href='/' className='font-semibold text-4xl text-pink-500 hover:text-violet-500 duration-300 flex items-center gap-4 flex-wrap max-w-max'>
                 Start Now
-                <IoIosArrowDropright />
+                <IoIosArrowDropright className='self-end'/>
             </a>
         </div>
 
