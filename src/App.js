fa069831b432b689d67af2911662bbc35e161781
Simone823 <simone.daglio823@outlook.it>
2022-11-12 20:51:40 +0100

    header, footer, route cart shop

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import './App.css';
 // import router dom
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+// import header components
+import Header from './components/Header';
+
+// import footer components
+import Footer from './components/Footer';
+
 // import homepage pages
 import Homepage from './pages/Homepage';
+
+// import cart shop pages
+import CartShop from './pages/CartShop';
 
 function App() {
   return (
     <Router>
+
+      {/* header */}
+      <Header/>
       
       {/* main */}
-      <main>
+      <main className='bg-zinc-900 text-white'>
         <Routes>
           <Route path='/' element={<Homepage />} />
+          <Route path='/cart-shop' element={<CartShop/>} />
         </Routes>
       </main>
-      
+
+      {/* footer */}
+      <Footer/>
+
     </Router>
   );
 }
