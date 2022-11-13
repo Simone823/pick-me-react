9bf6a9c166e976c3c7879d9f32dc9568f35618d0
Simone823 <simone.daglio823@outlook.it>
2022-11-13 01:00:39 +0100

    fix text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,9 +8,9 @@ function Hero() {
     <div className='hero grid grid-cols-1 sm:grid-cols-2 gap-6 gap-y-10'>
         {/* text */}
         <div className='text-hero'>
-            <h1 className='font-bold text-5xl sm:text-7xl mb-16'>The easiest way to buy Photos as NFT.</h1>
+            <h1 className='font-bold text-5xl sm:text-7xl mb-16'>Il modo più semplice per acquistare foto come NFT.</h1>
             <a href='/' className='font-semibold text-4xl text-pink-500 hover:text-violet-500 duration-300 flex items-center gap-4 flex-wrap max-w-max'>
-                Start Now
+                Inizia Ora
                 <IoIosArrowDropright className='self-end'/>
             </a>
         </div>
