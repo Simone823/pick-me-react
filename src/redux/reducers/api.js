50acbaf7d3423b9e9fbbdb6ba123625768e953fa
Simone823 <simone.daglio823@outlook.it>
2022-11-15 11:56:08 +0100

    redux reducers api

---
+++ src/redux/reducers/api.js
@@ -0,0 +1,65 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+// import instance custom axios
+import instance from '../../api';
+
+// initial state api
+const initialState = {
+    loading: true,
+    error: {
+        status: false,
+        message: ''
+    },
+    photos: []
+};
+
+// api slice
+const apiSlice = createSlice({
+    name: 'api',
+    initialState,
+    reducers: {
+        startLoading: (state) => {
+            state.loading = true;
+            state.error.status = false;
+            state.error.message = "";
+            state.photos = [];
+        },
+
+        stopLoading: (state) => {
+            state.loading = false;
+        },
+
+        saveData: (state, action) => {
+            state.photos = action.payload;
+        },
+
+        errorCatch: (state, action) => {
+            state.error.status = true;
+            state.error.message = action.payload;
+        }
+    }
+});
+
+// actions apiSlice
+const { startLoading, stopLoading, saveData, errorCatch } = apiSlice.actions;
+
+// fetch data api
+const fetchData = (path) => (dispatch) => {
+    dispatch(startLoading);
+    
+    // intance get
+    instance.get(path)
+    .then((res) => {
+        dispatch(saveData(res.data));
+        dispatch(stopLoading());
+    })
+    .catch((err) => {
+        dispatch(errorCatch(err.errors));
+        dispatch(stopLoading());
+    })
+}
+export {fetchData};
+
+// export reducers apiSlice
+const {reducer} = apiSlice;
+export default reducer;
