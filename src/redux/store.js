47c5754b9bd83ca7743dc466de5ac6fe11dcc751
Simone823 <simone.daglio823@outlook.it>
2022-11-21 16:30:38 +0100

    import cart store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,14 @@ import {configureStore } from '@reduxjs/toolkit';
 
 import apiReducer from '../redux/reducers/api';
 
+// import cart reducer
+import cartReducer from '../redux/reducers/cart';
+
 // store
 const store = configureStore({
     reducer: {
         photos: apiReducer,
+        cartShop: cartReducer
     }
 });
 
