78d2e3825b3dba6c76717886e0432cc434cc8a82
Simone823 <simone.daglio823@outlook.it>
2022-11-23 11:38:59 +0100

    total price localstorage

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -28,23 +28,63 @@ const cartSlice = createSlice({
                 }
             }
 
+            // somma prezzo stesso prodotto
+            let sumItem;
+
+            // somma totale prezzo
+            let totalCart = 0;
+
+            // for state cart
+            for (let i = 0; i < state.cart.length; i++) {
+                sumItem = state.cart[i].likes * 1;
+                totalCart += sumItem;
+            }
+
+            // state total
+            state.total = totalCart;
+
             // set item localStorage
             localStorage.setItem('cart', JSON.stringify(state.cart));
+
+            // localStorage set item total 
+            localStorage.setItem('total', totalCart);
         },
 
         removeToCart: (state, action) => {
             state.cart = state.cart.filter((el) => el.id !== action.payload);
 
+            // somma prezzo stesso prodotto
+            let sumItem;
+
+            // somma totale prezzo
+            let totalCart = 0;
+
+            // for state cart
+            for (let i = 0; i < state.cart.length; i++) {
+                sumItem = state.cart[i].likes * 1;
+                totalCart += sumItem;
+            }
+
+            // state total 
+            state.total = totalCart;
+
             // set cart localStorage
             localStorage.setItem('cart', JSON.stringify(state.cart));
+
+            // localStorage set item total 
+            localStorage.setItem('total', totalCart);
         },
 
         removeAllToCart: (state) => {
             // reset
             state.cart = [];
+            state.total = 0;
 
             // localStorage cart
             localStorage.setItem('cart', JSON.stringify(state.cart));
+
+            // localStorage set total
+            localStorage.setItem('total', state.total);
         }
     }
 });
